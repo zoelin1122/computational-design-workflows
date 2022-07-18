309336216a5529a2037985cac6cb6d73740b521d
zoelin1122 <87543028+zoelin1122@users.noreply.github.com>
2022-07-17 22:58:42 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("this thing works");
+const John = {firstName:"John", lastName:"Doe", age:50, height:5.6};
+const Zoe = {firstName:"Zoe", lastName:"Lin", age:7, height:4};
+const Seth = {firstName:"Seth", lastName:"Johnson", age:22, height:4.3};
+const Marry = {firstName:"Marry", lastName:"Joe", age:80, height:5.6};
+
+
+function whatToDo (person){
+    if (person.age < 8) {
+        console.log("Check out the Marry-Go-Around!");
+    } else if (person.age < 65 && person.height < 4.5){
+        console.log("Check out the rollercoaster!");
+    } else {
+        console.log("Check out the lazy-river!");
+    }
+}
+
