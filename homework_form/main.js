7fe56e5a4b67af966a2b7dba14a0cc8d37270b02
zoelin1122 <87543028+zoelin1122@users.noreply.github.com>
2022-08-04 00:33:19 -0400

    Update main.js

--- homework_form/main.js
+++ homework_form/main.js
@@ -14,6 +14,21 @@ let bathrooms = {
 	"bathroomList": []
 }
 
+let data = [
+    {
+        location: [-73.96191,40.80762],
+        content: 'I like to eat my lunch here'
+    },
+    {
+        location: [-73.95936,40.80610],
+        content: '15 years ago, you could see over the trees'
+    },
+    {
+        location: [-73.96204,40.80994],
+        content: 'This was once tennis courts'
+    },
+    ]
+
 
 // Event Handlers
 //----------------------------
@@ -287,6 +302,85 @@ function pageLoadFn(){
 }
 
 pageLoadFn()
+
+
+
+
+// MAP RELATED JS
+//-----------------
+// create an instance of NavigationControl
+let navigation = new mapboxgl.NavigationControl({
+    showCompass: false
+})
+
+// add the navigation to your map
+map.addControl(navigation, 'top-left')
+
+// create an instance of ScaleControl
+let scale = new mapboxgl.ScaleControl({
+    maxWidth: 80,
+    unit: 'imperial'
+})
+
+// add the scale to your map
+map.addControl(scale, 'bottom-right')
+
+let geolocate = new mapboxgl.GeolocateControl({
+    positionOptions: {
+        enableHighAccuracy: true
+    },
+    trackUserLocation: true,
+    showUserLocation: true,
+    fitBoundsOptions: {
+    }
+})
+
+map.addControl(geolocate, 'top-left')
+
+// this is an event handler
+geolocate.on('geolocate', function(event) {
+
+    // create new variables to store the attributes we're interested in from the event
+    let lng = event.coords.longitude
+    let lat = event.coords.latitude
+
+    // debug
+    console.log('geolocated:', lng, lat)
+
+    // format lng lat values and display them on our 'info' element
+    document.getElementById('info').innerHTML = lng.toFixed(5) + "," + lat.toFixed(5)
+
+})
+
+map.on('click', function(event) {
+
+    let lng = event.lngLat.lng
+    let lat = event.lngLat.lat
+
+    console.log("clicked:", lng, lat)
+
+    document.getElementById('info').innerHTML = lng.toFixed(5) + "," + lat.toFixed(5)
+
+})
+
+
+function renderLocations(locations){
+    locations.forEach(function(d) {
+
+        let marker = new mapboxgl.Marker()    
+        marker.setLngLat(d.location)
+        marker.addTo(map)  
+    
+        let popup = new mapboxgl.Popup()
+        popup.setHTML(d.content)
+        marker.setPopup(popup)
+    
+    })
+}
+
+renderLocations(data);
+
+
 
 
 // Events
